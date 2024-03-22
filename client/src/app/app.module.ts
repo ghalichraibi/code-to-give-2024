00245ea72d92bc77e3d6d4def50778ea3961a5d7
Ghali Chraibi <chraibi.ghali99@gmail.com>
2024-03-22 11:43:24 -0400

    initial angular client

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from '@app/modules/app-routing.module';
+import { AppMaterialModule } from '@app/modules/material.module';
+import { AppComponent } from '@app/pages/app/app.component';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+
+/**
+ * Main module that is used in main.ts.
+ * All automatically generated components will appear in this module.
+ * Please do not move this module in the module folder.
+ * Otherwise Angular Cli will not know in which module to put new component
+ */
+@NgModule({
+    declarations: [AppComponent, MainPageComponent],
+    imports: [AppMaterialModule, AppRoutingModule, BrowserAnimationsModule, BrowserModule, FormsModule, HttpClientModule],
+    providers: [],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ client/src/app/modules/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: MainPageComponent },
+    { path: '**', redirectTo: '/home' },
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
