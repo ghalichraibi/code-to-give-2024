587e779404861b4071d000b0187565cba1228678
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-03-23 22:19:19 -0400

    OB | resident list

--- client/src/app/components/residents/residents.component.ts
+++ client/src/app/components/residents/residents.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-residents',
   templateUrl: './residents.component.html',
   styleUrls: ['./residents.component.scss']
 })
-export class ResidentsComponent {
+export class ResidentsComponent implements OnInit {
+  @Output() openChatRequest = new EventEmitter<any>();
 
+  residents: any[] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.fetchResidents();
+  }
+
+  fetchResidents() {
+    this.residents = [
+      { name: 'John Doe', age: 65, gender: 'Male' },
+      { name: 'Jane Smith', age: 72, gender: 'Female' },
+    ];
+  }
+
+  openChat(resident: any) {
+    this.openChatRequest.emit(resident);
+  }
+
+  modifyInfo(resident: any) {
+    console.log('Modifying info of resident:', resident);
+  }
+
+  viewDocuments(resident: any) {
+    console.log('Viewing documents of resident:', resident);
+  }
 }
