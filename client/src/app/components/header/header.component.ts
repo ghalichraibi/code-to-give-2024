8fe419397f7bc60995875f0dec55ebf3d6d60881
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-03-24 22:08:25 -0400

    OB | ui baby

--- client/src/app/components/header/header.component.ts
+++ client/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  constructor(private router: Router){}
+
+  isResident(): boolean {
+    return this.router.url.includes('resident');
+  }
 }
