cc68b9018a228a72f0de309cc4d8e94da357f699
Omar Benzekri <omar.benzekri.2003@gmail.com>
2024-03-24 22:54:31 -0400

    OB | profile info

--- client/src/app/components/personal-information/personal-information.component.ts
+++ client/src/app/components/personal-information/personal-information.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { residentStub, caregiverStub } from '@common/stubs/residentStubs';
 
 @Component({
   selector: 'app-personal-information',
@@ -6,5 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./personal-information.component.scss']
 })
 export class PersonalInformationComponent {
+    currentResident = residentStub;
+    currentCaregiver = caregiverStub;
 
+    displayedInformation = [
+      { label: 'Last name', value: this.currentResident.lastName },
+      { label: 'First name', value: this.currentResident.firstName },
+      { label: 'Birth Date', value: this.currentResident.birthDate.toLocaleDateString() },
+      { label: 'Place of accomodation', value: this.currentResident.currentLodging },
+      { label: 'Immigration Status', value: this.currentResident.immigrationStatus },
+      { label: 'Number of children', value: this.currentResident.numberOfChildren },
+      { label: 'Issues', value: this.currentResident.issues },
+      { label: 'Monthly income', value: this.currentResident.monthlyIncome + '$'},
+      { label: 'Native', value: this.currentResident.isIndigenous? 'Yes': 'No' },
+      { label: 'Veteran', value: this.currentResident.isVeteran? 'Yes' : 'No' }
+    ];
+    contactDetails = [
+      {label: 'Email', value: this.currentResident.email},
+      {label: 'Phone Number', value: this.currentResident.phoneNumber},
+      {label: 'Borough', value: this.currentResident.borough}
+    ];
+
+    caregiverDetails = [
+      {label: 'full name', value: this.currentCaregiver.firstName +' '+  this.currentCaregiver.lastName},
+      {label: 'Email', value: this.currentCaregiver.email},
+      {label: 'Phone Number', value: this.currentCaregiver.phoneNumber}
+    ];
 }
