369ca7519e4934c7b28c91378e56b4f38300a212
ihsane03 <ihsane.majdoubi@polymtl.ca>
2024-03-24 21:45:04 -0400

    added significant person stub

--- common/stubs/residentStub.ts
+++ common/stubs/residentStub.ts
@@ -2,6 +2,7 @@ import { Resident, Caregiver } from "../interfaces/stakeholders/users";
 import { UserRoles } from "../enums/user-roles.enum";
 import { ImmigrationStatus } from "../enums/immigration-status.enum";
 import { Issue } from "../enums/issue.enum";
+import { PersonRole, RelationType, SignificantPerson } from "../interfaces/stakeholders/persons";
 
 export const caregiverStub: Caregiver = {
     id: "1",
@@ -17,6 +18,14 @@ export const caregiverStub: Caregiver = {
     manager: "0",
   };
 
+const significantPersonStub: SignificantPerson = {
+    name: "Jane",
+    phoneNumber: "123-456-7890",
+    email: "Jane@gmail.com",
+    relationType: RelationType.Family,
+    role: PersonRole.Significant
+}
+
 export const residentStub: Resident = {
     id: "2",
     role: UserRoles.Resident,
@@ -27,7 +36,7 @@ export const residentStub: Resident = {
     phoneNumber: "123-456-7890",
     hashedPassword: "$2b$10$1TfwRUWLBWd9lf9pwmKnceT.ZHV0PcnB/k6P6xAI5enWqSWLZ9l/O", // test
     accessibleDocuments: [],
-    significantPeople: [],
+    significantPeople: [significantPersonStub],
     monthlyIncome: 1000,
     borough: "Cote-des-Neiges",
     issues: [Issue.Homelessness],
