fb6e90bd30b943b479c2885fd250241d19c5271c
ihsane03 <ihsane.majdoubi@polymtl.ca>
2024-03-24 21:02:41 -0400

    stub added

--- common/stubs/residentStub.ts
+++ common/stubs/residentStub.ts
@@ -1,7 +1,21 @@
-import { Resident } from "../interfaces/stakeholders/users";	
+import { Resident, Caregiver } from "../interfaces/stakeholders/users";	
 import { UserRoles } from "../enums/user-roles.enum";
 import { ImmigrationStatus } from "../enums/immigration-status.enum";
 import { Issue } from "../enums/issue.enum";
+
+export const caregiverStub: Caregiver = {
+    id: "1",
+    role: UserRoles.Caregiver,
+    email: "test@test.com",
+    firstName: "Lovely",
+    lastName: "Person",
+    phoneNumber: "123-456-7890",
+    hashedPassword:
+      "$2b$10$1TfwRUWLBWd9lf9pwmKnceT.ZHV0PcnB/k6P6xAI5enWqSWLZ9l/O", // test
+    residents: ["2"],
+    accessibleDocuments: [],
+    manager: "0",
+  };
 
 export const residentStub: Resident = {
     id: "2",
