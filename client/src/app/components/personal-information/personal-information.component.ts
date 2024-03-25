26ad421a079dbf7f121d67428f1f54c0b589a78d
Ghali Chraibi <chraibi.ghali99@gmail.com>
2024-03-25 06:29:58 -0400

    Replaced harcoded personal information by fetched data

--- client/src/app/components/personal-information/personal-information.component.ts
+++ client/src/app/components/personal-information/personal-information.component.ts
@@ -1,36 +1,66 @@
-import { Component } from '@angular/core';
-import { residentStub, caregiverStub } from '@common/stubs/residentStubs';
+import { Component } from "@angular/core";
+import { UserService } from "@app/services/user.service";
+import { Resident } from "@common/interfaces/stakeholders/users";
+import { residentStub, caregiverStub } from "@common/stubs/residentStubs";
 
 @Component({
-  selector: 'app-personal-information',
-  templateUrl: './personal-information.component.html',
-  styleUrls: ['./personal-information.component.scss']
+  selector: "app-personal-information",
+  templateUrl: "./personal-information.component.html",
+  styleUrls: ["./personal-information.component.scss"],
 })
 export class PersonalInformationComponent {
-    currentResident = residentStub;
-    currentCaregiver = caregiverStub;
+  residentStub = residentStub;
+  currentCaregiver = caregiverStub;
 
-    displayedInformation = [
-      { label: 'Last name', value: this.currentResident.lastName },
-      { label: 'First name', value: this.currentResident.firstName },
-      { label: 'Birth Date', value: this.currentResident.birthDate.toLocaleDateString() },
-      { label: 'Place of accomodation', value: this.currentResident.currentLodging },
-      { label: 'Immigration Status', value: this.currentResident.immigrationStatus },
-      { label: 'Number of children', value: this.currentResident.numberOfChildren },
-      { label: 'Issues', value: this.currentResident.issues },
-      { label: 'Monthly income', value: this.currentResident.monthlyIncome + '$'},
-      { label: 'Native', value: this.currentResident.isIndigenous? 'Yes': 'No' },
-      { label: 'Veteran', value: this.currentResident.isVeteran? 'Yes' : 'No' }
-    ];
-    contactDetails = [
-      {label: 'Email', value: this.currentResident.email},
-      {label: 'Phone Number', value: this.currentResident.phoneNumber},
-      {label: 'Borough', value: this.currentResident.borough}
-    ];
+  constructor(private userService: UserService) {}
 
-    caregiverDetails = [
-      {label: 'full name', value: this.currentCaregiver.firstName +' '+  this.currentCaregiver.lastName},
-      {label: 'Email', value: this.currentCaregiver.email},
-      {label: 'Phone Number', value: this.currentCaregiver.phoneNumber}
-    ];
+  get currentResident(): Resident {
+    return this.userService.user as any as Resident;
+  }
+
+  displayedInformation = [
+    { label: "Last name", value: this.currentResident.lastName },
+    { label: "First name", value: this.currentResident.firstName },
+    {
+      label: "Birth Date",
+      value: this.residentStub.birthDate.toLocaleDateString(),
+    },
+    {
+      label: "Place of accomodation",
+      value: this.currentResident.currentLodging,
+    },
+    {
+      label: "Immigration Status",
+      value: this.currentResident.immigrationStatus,
+    },
+    {
+      label: "Number of children",
+      value: this.currentResident.numberOfChildren,
+    },
+    { label: "Issues", value: this.currentResident.issues },
+    {
+      label: "Monthly income",
+      value: this.currentResident.monthlyIncome + "$",
+    },
+    {
+      label: "Native",
+      value: this.currentResident.isIndigenous ? "Yes" : "No",
+    },
+    { label: "Veteran", value: this.currentResident.isVeteran ? "Yes" : "No" },
+  ];
+  contactDetails = [
+    { label: "Email", value: this.currentResident.email },
+    { label: "Phone Number", value: this.currentResident.phoneNumber },
+    { label: "Borough", value: this.currentResident.borough },
+  ];
+
+  caregiverDetails = [
+    {
+      label: "full name",
+      value:
+        this.currentCaregiver.firstName + " " + this.currentCaregiver.lastName,
+    },
+    { label: "Email", value: this.currentCaregiver.email },
+    { label: "Phone Number", value: this.currentCaregiver.phoneNumber },
+  ];
 }
